471e0e118c083ebc5905127bf506302abf8e756a
Sasaki Touma <netst915@gmail.com>
2024-07-04 00:06:32 +0900

    minimise bundle.js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,10 @@ const { Elm } = require("./Main.elm");
 
 Elm.Main.init();
 
-const hljs = require("highlight.js/lib/core");
-const haskellLang = require("highlight.js/lib/languages/haskell");
+window.addEventListener("load", jsMain);
 
-hljs.registerLanguage("haskell", haskellLang);
-hljs.highlightAll();
-
-import renderMathInElement from "katex/contrib/auto-render/auto-render";
-renderMathInElement(document.body);
+function jsMain()
+{
+    hljs.highlightAll();
+    renderMathInElement(document.body);
+}
